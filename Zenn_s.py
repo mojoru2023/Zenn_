1c8f2417ca8fd8615e386d630bf7845bdc2c6e9e
MOJORU <34601389+mojoru2023@users.noreply.github.com>
2023-05-17 23:52:09 +0900

    Update Zenn_s.py

--- Zenn_s.py
+++ Zenn_s.py
@@ -1,80 +1,14 @@
-#
-#
-# # 1. read the folder , to output json file ,
-#
-#
-#
+
+import sys
+
+
+
+import csv
+import math
 import math
 
 import requests
 from lxml import etree
-
-
-
-
-class CSBookOnePage(object):
-
-    def __init__(self):
-        pass
-
-
-    def read_folder_output_template(self):
-
-        """
-
-        return:  1.urls_list , 2.output pro_template.html
-        """
-        # 1 读取文件目录，生成json格式的层级内容。同时剔除一些比如 .md 的内容，同时生成template
-        # pro_template.html
-        pass
-
-
-    def pro_topContents_html(self):
-        # 2 继承模板页面，生成项目目录contents的首页
-        pass
-        # 3 读取目录的文件内容，生成单页面的URL，请求后整理，并生成继承模板的单一文件的html文件。
-
-    @staticmethod
-    def read_folder(folderpath):
-        pass
-
-    def remove_get_rid_list(self,list_content):
-        ret_list = []
-        for item in list_content:
-            for remove_item in get_rid_list:
-                if remove_item in item:
-                    ret_list.append(item)
-                    # list_content.remove(item)
-        [list_content.remove(x) for x in ret_list]
-        return list_content
-
-    def __recursive(self,url):
-        ret = requests.get(url)
-        element = etree.HTML(ret.text)
-
-        article_nums = element.xpath('//*[@id="__next"]/header[2]/div/nav/a[1]/span/text()')
-        article_title = element.xpath('//*[@id="__next"]/div[1]/div/section/div[2]/div/div/article/div/a/h2/text()')
-        article_urls = element.xpath('//*[@id="__next"]/div[1]/div/section/div[2]/div/div/article/div/a/@href')
-        f_article_urls = ["https://zenn.dev{0}".format(x) for x in article_urls]
-
-        pages = math.ceil(float(int(article_nums[0])/48))
-
-        for item in urls:
-
-            if "tree" not in item:
-                file_list.append("https://github.com{0}".format(item))
-                print(item)
-            elif "tree" in item and item not in folder_list:
-                self.__recursive("https://github.com{0}".format(item))
-
-    def last_name_htmlfile(self,url):
-        # 借鉴其他的网站，只保留最后的文件即可
-        htmlfile = url.split("/")[-1]+".html"
-
-        return htmlfile
-
-
-
 # ab -n 10000 -c 100  http://localhost:8080/
 
 # 介绍python获取命令行参数的方法：getopt模和argparse模块。
@@ -82,12 +16,62 @@ import os
 import sys
 import getopt
 
-
-
-
 import getopt
 import sys
 
+
+import time
+from requests.exceptions import ConnectionError
+from selenium import webdriver
+from lxml import etree
+import datetime
+driver = webdriver.Chrome()
+
+
+
+
+
+def writeintoTSV_file(filename, data):
+    with open(filename, 'a', encoding="utf-8") as f_output:
+        tsv_output = csv.writer(f_output, delimiter='\t')
+        tsv_output.writerow(data)
+def use_selenium_request(url):
+
+    driver.get(url)
+    # time.sleep(3)
+    html = driver.page_source
+    # time.sleep(3)
+    return html
+
+def exec(topicurl):
+    first_url = "https://zenn.dev/topics/{0}?page=1".format(topicurl)
+
+    ret = use_selenium_request(first_url)
+    element = etree.HTML(ret)
+
+    article_nums = element.xpath('//*[@id="__next"]/header[2]/div/nav/a[1]/span/text()')
+    article_title = element.xpath('//*[@id="__next"]/div[1]/div/section/div[2]/div/div/article/div/a/h2/text()')
+    article_urls = element.xpath('//*[@id="__next"]/div[1]/div/section/div[2]/div/div/article/div/a/@href')
+    f_article_urls = ["https://zenn.dev{0}".format(x) for x in article_urls]
+
+    for i1, i2 in zip(article_title, f_article_urls):
+        one_str = str(i1 + " " + i2)
+        writeintoTSV_file(topicurl + ".tsv", [one_str])
+
+    f_nums = math.ceil(int(article_nums[0]) /48)
+
+    for item in range(2,f_nums + 1):
+        other_url = "https://zenn.dev/topics/{0}?page={1}".format(topicurl, str(item))
+        ret = use_selenium_request(other_url)
+        element = etree.HTML(ret)
+
+        article_title = element.xpath('//*[@id="__next"]/div[1]/div/section/div[2]/div/div/article/div/a/h2/text()')
+        article_urls = element.xpath('//*[@id="__next"]/div[1]/div/section/div[2]/div/div/article/div/a/@href')
+        f_article_urls = ["https://zenn.dev{0}".format(x) for x in article_urls]
+
+        for i1, i2 in zip(article_title, f_article_urls):
+            one_str = str(i1 + " " + i2)
+            writeintoTSV_file(topicurl + ".tsv", [one_str])
 
 def main(argv):
     """
@@ -96,30 +80,16 @@ def main(argv):
     :param argv:
     :return:
     """
-    topicurl =""
+    topicurl = ""
 
     try:
-        options, args = getopt.getopt(argv, "ht:c:a:", ['help',"topic="])
+        options, args = getopt.getopt(argv, "ht:c:a:", ['help', "topic="])
 
         for option, value in options:
             if option in ("-t", "--topic"):
-                topicurl  = value
+                topicurl = value
 
-        first_url = "https://zenn.dev/topics/{}?page=1".format(topicurl)
-
-        ret = requests.get(first_url)
-        element = etree.HTML(ret.text)
-
-        article_nums = element.xpath('//*[@id="__next"]/header[2]/div/nav/a[1]/span/text()')
-        article_title = element.xpath('//*[@id="__next"]/div[1]/div/section/div[2]/div/div/article/div/a/h2/text()')
-        article_urls = element.xpath('//*[@id="__next"]/div[1]/div/section/div[2]/div/div/article/div/a/@href')
-        f_article_urls = ["https://zenn.dev{0}".format(x) for x in article_urls]
-
-        pages = math.ceil(float(int(article_nums[0])/48))
-
-
-        for i1,i2 in zip():
-            
+        exec(topicurl)
 
 
     except getopt.GetoptError:
@@ -127,7 +97,7 @@ def main(argv):
         print("\n")
         print("\n")
         print("\n")
-        print("python3 run_rust.py -r test.rs")
+        print("python3 Zenn_.py -t python")
         print("\n")
         print("\n")
         print("\n")
@@ -139,5 +109,7 @@ def main(argv):
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+    sys.exit(1)
 
 
+    
